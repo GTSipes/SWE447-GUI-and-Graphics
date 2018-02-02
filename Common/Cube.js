456bb08ea99dee3056545a4b65e0407a177189e6
Galen Sipes <gtsipes97@gmail.com>
2018-02-02 00:04:44 -0800

    Working on assignment 4

---
+++ Common/Cube.js
@@ -0,0 +1,77 @@
+
+function Cube( vertexShaderId, fragmentShaderId ) {
+
+    // Initialize the shader pipeline for this object using either shader ids
+    //   declared in the application's HTML header, or use the default names.
+    //
+    var vertShdr = vertexShaderId || "Cube-vertex-shader";
+    var fragShdr = fragmentShaderId || "Cube-fragment-shader";
+
+    this.program = initShaders(gl, vertShdr, fragShdr);
+
+    if ( this.program < 0 ) {
+        alert( "Error: Cube shader pipeline failed to compile.\n\n" +
+            "\tvertex shader id:  \t" + vertShdr + "\n" +
+            "\tfragment shader id:\t" + fragShdr + "\n" );
+        return; 
+    }
+
+    this.positions = { 
+        values : new Float32Array([
+           // Add your list vertex positions here
+		   	0.0, 0.0, 0.0,
+			1.0, 0.0, 0.0,
+			1.0, 1.0, 0.0,
+			0.0, 1.0, 0.0,
+			0.0, 0.0, 1.0,
+			1.0, 0.0, 1.0,
+			0.0, 1.0, 1.0,
+			1.1, 1.1, 1.1,
+            ]),
+        numComponents : 3
+    };
+    
+    this.indices = { 
+        values : new Uint16Array([
+            // Add your list of triangle indices here
+			0, 1, 2, //front face
+			2, 0, 3, //left side face
+			3, 2, 6, //back face
+			6, 2, 7, //
+			7, 2, 5,
+			5, 2, 1,
+        ])
+    };
+    this.indices.count = this.indices.values.length;
+
+    
+    this.positions.buffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, this.positions.buffer );
+    gl.bufferData( gl.ARRAY_BUFFER, this.positions.values, gl.STATIC_DRAW );
+
+    this.indices.buffer = gl.createBuffer();
+    gl.bindBuffer( gl.ELEMENT_ARRAY_BUFFER, this.indices.buffer );
+    gl.bufferData( gl.ELEMENT_ARRAY_BUFFER, this.indices.values, gl.STATIC_DRAW );
+
+    this.positions.attributeLoc = gl.getAttribLocation( this.program, "vPosition" );
+    gl.enableVertexAttribArray( this.positions.attributeLoc );
+
+    MVLoc = gl.getUniformLocation( this.program, "MV" );
+
+    this.MV = undefined;
+
+    this.render = function () {
+        gl.useProgram( this.program );
+
+        gl.bindBuffer( gl.ARRAY_BUFFER, this.positions.buffer );
+        gl.vertexAttribPointer( this.positions.attributeLoc, this.positions.numComponents,
+            gl.FLOAT, gl.FALSE, 0, 0 );
+ 
+        gl.bindBuffer( gl.ELEMENT_ARRAY_BUFFER, this.indices.buffer );
+
+        gl.uniformMatrix4fv( MVLoc, gl.FALSE, flatten(this.MV) );
+
+        // Draw the cube's base
+        gl.drawElements( gl.TRIANGLES, this.indices.count, gl.UNSIGNED_SHORT, 0 );
+    }
+};
